55390836d3334ceefa7d600c46481ebb8bad175e
Phạm Phương Anh <phamphuonganh.hvact@gmail.com>
2021-07-21 14:49:35 +0700

    csv

--- SOPJ/JavaIO.csv/src/javaio/csv/JavaIOCsv.java
+++ SOPJ/JavaIO.csv/src/javaio/csv/JavaIOCsv.java
@@ -5,17 +5,40 @@
  */
 package javaio.csv;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author phuon
  */
 public class JavaIOCsv {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final String currentDir = System.getProperty("user.dir");
+    private static final String separator = File.separator;
+    private static final String PATH_CSV_FILE = currentDir + separator + "data" + separator + "countries.csv";
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(PATH_CSV_FILE));
+            String line = null;
+            String[] cols = new String[4];
+            while ((line = br.readLine()) != null)  {
+                cols = line.split(",");
+//                System.out.println(cols[0]+","+ cols[1]+","+ cols[2]+","+cols[3]);
+                
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaIOCsv.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaIOCsv.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
+
 }
