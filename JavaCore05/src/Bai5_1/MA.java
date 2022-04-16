2ab41ef5c08c27b13413d4cc61f3183099c6aee1
PhuongAnh <phamphuonganh.hvact@gmail.com>
2022-04-16 18:10:20 +0700

    update

---
+++ JavaCore05/src/Bai5_1/MA.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai5_1;
+
+/**
+ *
+ * @author phuon
+ */
+public class MA extends SanPham{
+    int ma; 
+    
+}
